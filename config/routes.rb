ce6d82463cec2d5a646b2c5e033e1dfb056fb2ac
Cornelius Heggli <cornelius.heggli@gmail.com>
2022-11-08 16:54:08 +0800

    Done with the bloody routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,18 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # As a user, I can list tasks
+  root to: 'tasks#index'
+  # get "tasks/new", to: "tasks#new"
+  # post "tasks", to: "tasks#create"
+  # get "tasks/:id", to: "tasks#show", as: :task
+  # get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+  # patch "tasks/:id", to: "tasks#update"
+  # delete "tasks/:id", to: "tasks#destroy"
+  resources :tasks, except: :index
 end
+
+# As a user, I can view the details of a task
+# As a user, I can add a new task
+# As a user, I can edit a task (mark as completed / update title & details)
+# As a user, I can remove a task
